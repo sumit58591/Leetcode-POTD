cb2de5d361d576d62de79dc51c1e02ab1ff9e40b
Sumit Verma <sumitvr75@gmail.com>
2025-05-28 00:47:53 +0530

    2025, May 27th POTD

---
+++ Leetcode_2894.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int differenceOfSums(int n, int m) {
+    int num1 = 0, num2 = 0;
+    for(int i = 1; i <= n; i++) {
+        if(i % m != 0) num1 += i;
+        else num2 += i;
+    }
+
+    return num1 - num2;
+}
+
+int main() {
+    int n, m;
+    cin >> n >> m;
+    int differenceOfSumsOutput = differenceOfSums(n, m);
+    cout << differenceOfSumsOutput << endl;
+    return 0;
+}
